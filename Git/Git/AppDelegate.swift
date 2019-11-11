f62ae0784a14ac3292e6d8d77138579acf86e6f1
wrwrwr1 <z00000gg@163.com>
2019-11-11 18:29:29 +0800

    First Commit

---
+++ Git/Git/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Git
+//
+//  Created by 111 on 2019/11/11.
+//  Copyright © 2019 ZouYun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Git/Git/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by 111 on 2019/11/11.
+//  Copyright © 2019 ZouYun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
